8bcff480cd8f1e437a0082bc6765df9f7dbec052
Tiago Almeida <almeidatiago56@gmail.com>
2022-05-26 12:05:52 +0100

    Fix UI test due to Recyclerview assertion

--- app/src/androidTest/java/com/task/noteapp/utils/RecyclerViewItemCountAssertion.kt
+++ app/src/androidTest/java/com/task/noteapp/utils/RecyclerViewItemCountAssertion.kt
@@ -8,7 +8,10 @@ import androidx.test.espresso.matcher.ViewMatchers
 import org.hamcrest.Matchers
 
 class RecyclerViewItemCountAssertion(private val expectedCount: Int) : ViewAssertion {
-    override fun check(view: View, noViewFoundException: NoMatchingViewException) {
+    override fun check(view: View, noViewFoundException: NoMatchingViewException?) {
+        if (noViewFoundException != null) {
+            throw noViewFoundException
+        }
         val recyclerView = view as RecyclerView
         val adapter = recyclerView.adapter
         ViewMatchers.assertThat(
